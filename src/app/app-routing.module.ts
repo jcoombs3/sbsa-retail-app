902867fc7d6da37f1dce6b743741927ae7c45b60
John Coombs <johnc@backbase.com>
2021-11-04 21:01:50 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "my-accounts",
+    loadChildren: () =>
+      import(
+        "./accounts-transactions/accounts-transactions-journey-bundle.module"
+      ).then((m) => m.AccountsTransactionsJourneyBundleModule),
+  },
+  {
+    path: "**",
+    redirectTo: "my-accounts",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
