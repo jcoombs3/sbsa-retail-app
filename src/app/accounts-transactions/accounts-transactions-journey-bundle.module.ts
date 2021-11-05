27bef8545a77493bb6dc463b1a88cafc50882e84
John Coombs <johnc@backbase.com>
2021-11-04 21:12:26 -0400

    fake PubSub

--- src/app/accounts-transactions/accounts-transactions-journey-bundle.module.ts
+++ src/app/accounts-transactions/accounts-transactions-journey-bundle.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { TemplateRegistry, ItemModel } from "@backbase/foundation-ang/core";
 import { AccountsTransactionsJourneyModule } from "@backbase/accounts-transactions-journey-ang";
 import { of } from "rxjs";
+import { PUBSUB } from "@backbase/foundation-ang/web-sdk";
 
 export class CustomItemModel {
   constructor() {}
@@ -11,12 +12,47 @@ export class CustomItemModel {
   }
 }
 
+export class CustomPubsub {
+  subscriptions = {} as any;
+
+  constructor() {}
+
+  publish(eventName: string, data: any) {
+    console.log('++ eventName', eventName);
+    if (this.subscriptions[eventName]) {
+      this.subscriptions[eventName].forEach(function(listener: any) {
+        listener(data);
+      });
+    }
+  }
+  
+  subscribe(eventName: string, listener: any) {
+    console.log('++ subscribe', eventName);
+    this.subscriptions[eventName] = this.subscriptions[eventName] || [];
+    this.subscriptions[eventName].push(listener);
+  }
+  
+  unsubscribe(eventName: string, listener: any) {
+    console.log('++ unsubscribe', eventName);
+    const eventListeners = this.subscriptions[eventName];
+    if (eventListeners) {
+      eventListeners.splice(eventListeners.indexOf(listener), 1);
+    }
+  }
+}
+
 @NgModule({
   imports: [AccountsTransactionsJourneyModule.forRoot()],
-  providers: [TemplateRegistry, 
+  providers: [
+    TemplateRegistry,
     {
       provide: ItemModel,
       useClass: CustomItemModel
-    }],
+    },
+    {
+      provide: PUBSUB,
+      useClass: CustomPubsub
+    }
+  ]
 })
 export class AccountsTransactionsJourneyBundleModule {}
