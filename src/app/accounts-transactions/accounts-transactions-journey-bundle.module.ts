cdc6adbe676b0329cf2626931fad26ead4a5604b
John Coombs <johnc@backbase.com>
2021-11-10 10:02:43 -0500

    Update accounts-transactions-journey-bundle.module.ts

--- src/app/accounts-transactions/accounts-transactions-journey-bundle.module.ts
+++ src/app/accounts-transactions/accounts-transactions-journey-bundle.module.ts
@@ -1,10 +1,68 @@
 import { NgModule, Provider } from '@angular/core';
 import { TemplateRegistry } from '@backbase/foundation-ang/core';
-import { AccountsTransactionsJourneyModule } from '@backbase/accounts-transactions-journey-ang';
+import {
+  AccountsTransactionsJourneyModule,
+  AccountsTransactionsJourneyComponent,
+  AccountsListComponent,
+  AccountsManageComponent,
+  AccountsManageGuardService,
+  AccountsDetailsTabComponent,
+  TransactionsListComponent,
+  TransactionDetailsComponent,
+  AccountsDetailsComponent,
+} from '@backbase/accounts-transactions-journey-ang';
 import { providers } from './config.providers';
 
+const extendedRoute = {
+  path: '',
+  component: AccountsTransactionsJourneyComponent,
+  children: [
+    {
+      path: '',
+      redirectTo: 'list',
+      pathMatch: 'full',
+    },
+    {
+      path: 'list',
+      component: AccountsListComponent,
+      data: { title: 'My Accounts' },
+    },
+    {
+      path: 'manage',
+      component: AccountsManageComponent,
+      canActivate: [AccountsManageGuardService],
+    },
+    {
+      path: 'transactions',
+      component: AccountsDetailsTabComponent,
+      data: { title: 'Transactions' },
+      children: [
+        { path: '', redirectTo: 'list', pathMatch: 'full' },
+        {
+          path: 'list',
+          component: TransactionsListComponent,
+          data: { title: 'Transactions' },
+          children: [
+            {
+              path: 'detail',
+              component: TransactionDetailsComponent,
+            },
+          ],
+        },
+        {
+          path: 'details',
+          component: AccountsDetailsComponent,
+          data: { title: 'Details' },
+        },
+      ],
+    },
+  ],
+};
+
 @NgModule({
-  imports: [AccountsTransactionsJourneyModule.forRoot()],
+  imports: [
+    AccountsTransactionsJourneyModule.forRoot({ route: extendedRoute }),
+  ],
   providers: [
     ...providers,
     // extension slot dependency (WA3)
