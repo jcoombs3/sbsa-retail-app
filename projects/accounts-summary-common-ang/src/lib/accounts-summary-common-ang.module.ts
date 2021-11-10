270c3f54c77807d8a0679a6dab8b80d44770605a
John Coombs <johnc@backbase.com>
2021-11-10 11:42:23 -0500

    Revert "accounts-summary-common-ang"
    This reverts commit d50e6562cdc5dd8d07ca517a3ae969f8b0087f57.

--- projects/accounts-summary-common-ang/src/lib/accounts-summary-common-ang.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AccountsSummaryCommonAngComponent } from './accounts-summary-common-ang.component';
-
-@NgModule({
-  declarations: [AccountsSummaryCommonAngComponent],
-  imports: [],
-  exports: [AccountsSummaryCommonAngComponent],
-})
-export class AccountsSummaryCommonAngModule {}
--- projects/accounts-summary-common-ang/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of accounts-summary-common-ang
- */
-
-export * from './lib/accounts-summary-common-ang.service';
-export * from './lib/accounts-summary-common-ang.component';
-export * from './lib/accounts-summary-common-ang.module';
